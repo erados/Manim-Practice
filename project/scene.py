8156978f6efedcfb948373eea8293edf56d563bf
erados <imdiots@gmail.com>
2023-03-19 00:54:57 +0900

    Relative positioning

--- project/scene.py
+++ project/scene.py
@@ -19,3 +19,14 @@ class SquareToCircle(Scene):
         self.play(Create(square))  # animate the creation of the square
         self.play(Transform(square, circle))  # interpolate the square into the circle
         self.play(FadeOut(square))  # fade out animation
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        square.next_to(circle, RIGHT, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))  # show the shapes on screen
