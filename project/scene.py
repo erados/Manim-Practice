d4cb7dd8b40e22247959e77e6798d0d4d814653e
erados <imdiots@gmail.com>
2023-03-19 00:57:59 +0900

    Animate changes

--- project/scene.py
+++ project/scene.py
@@ -30,3 +30,17 @@ class SquareAndCircle(Scene):
 
         square.next_to(circle, RIGHT, buff=0.5)  # set the position
         self.play(Create(circle), Create(square))  # show the shapes on screen
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(
+            ReplacementTransform(square, circle)
+        )  # transform the square into a circle
+        self.play(
+            circle.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
